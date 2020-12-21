84a9c1027128038f0ed107678445d83e3cc57397
Aysha Maliha <ayshamaliha15@gmail.com>
2020-12-22 02:47:21 +0600

    UserAccess

---
+++ Digital Diary Version 2/UserDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Diary_Version_2
+{
+    class UserDataAccess
+    {
+        DataAccess dataAccess;
+        public UserDataAccess()
+        {
+            this.dataAccess = new DataAccess();
+        }
+
+        public bool LoginValidation(string username, string password)
+        {
+            string sql = "SELECT * FROM Users WHERE Username='" + username + "' AND Password='" + password + "'";
+            SqlDataReader reader = dataAccess.GetData(sql);
+            if (reader.Read())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
